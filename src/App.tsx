76e79633a60d201369b0194a55988c763fc987ce
program39 <jerielformento@gmail.com>
2025-03-26 20:43:34 +0800

    Setup page design

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [buttonName, setButtonName] = useState("Send");
+  const [delayInputValue, setDelayInputValue] = useState("");
+  const [slackWebHookUrl, setSlackWebhookUrl] = useState<string>("");
+
+  const handleSendMessage = async () => {
+    await fetch(slackWebHookUrl, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        text: "Hello World!",
+      }),
+    });
+  };
+
+  useEffect(() => {
+    setButtonName((prev) => `${prev} ${delayInputValue}`);
+  }, [delayInputValue, slackWebHookUrl]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col justify-center min-h-svh max-w-2xl mx-auto">
+      <Input onChange={(e) => setDelayInputValue(e.target.value)} />
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <label>Slack Hook URL Input</label>
+      <Input onChange={(e) => setSlackWebhookUrl(e.target.value)} />
+      <Button onClick={handleSendMessage}>{buttonName}</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
